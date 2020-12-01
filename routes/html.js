b82b16848f166bc61a90c6fb1ca6568c9e26fe7c
Josh Allan <allan.josh07@gmail.com>
2020-12-01 11:00:35 -0500

    Added express-handlebars as the view handler, and created a very basic initial view for the index page.

--- routes/html.js
+++ routes/html.js
@@ -1,46 +1,53 @@
 // required packages
 const axios = require('axios').default;
 
-//* Get request for the characters data
-app.get('/characters', (req, res) => {
-  axios
-    .get('https://breakingbadapi.com/api/characters')
-    .then((response) => {
-      console.log(response);
-      res.render('characters', { characters: response.data });
-    })
-    .catch((err) => console.log(err));
-});
+module.exports = (app) => {
+  //! GET request for the index page
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
 
-//* GET request for the episodes data
-app.get('/episodes', (req, res) => {
-  axios
-    .get('https://breakingbadapi.com/api/episodes')
-    .then((response) => {
-      console.log(response);
-      res.render('episodes', { episodes: response.data });
-    })
-    .catch((err) => console.log(err));
-});
+  //* Get request for the characters data
+  app.get('/characters', (req, res) => {
+    axios
+      .get('https://breakingbadapi.com/api/characters')
+      .then((response) => {
+        console.log(response);
+        res.render('characters', { characters: response.data });
+      })
+      .catch((err) => console.log(err));
+  });
 
-//* GET request for the quotes data
-app.get('/quotes', (req, res) => {
-  axios
-    .get('https://breakingbadapi.com/api/quotes')
-    .then((response) => {
-      console.log(response);
-      res.render('quotes', { quotes: response.data });
-    })
-    .catch((err) => console.log(err));
-});
+  //* GET request for the episodes data
+  app.get('/episodes', (req, res) => {
+    axios
+      .get('https://breakingbadapi.com/api/episodes')
+      .then((response) => {
+        console.log(response);
+        res.render('episodes', { episodes: response.data });
+      })
+      .catch((err) => console.log(err));
+  });
 
-//* GET request for the deaths data
-app.get('/deaths', (req, res) => {
-  axios
-    .get('https://breakingbadapi.com/api/deaths')
-    .then((response) => {
-      console.log(response);
-      res.render('deaths', { deaths: response.data });
-    })
-    .catch((err) => console.log(err));
-});
+  //* GET request for the quotes data
+  app.get('/quotes', (req, res) => {
+    axios
+      .get('https://breakingbadapi.com/api/quotes')
+      .then((response) => {
+        console.log(response);
+        res.render('quotes', { quotes: response.data });
+      })
+      .catch((err) => console.log(err));
+  });
+
+  //* GET request for the deaths data
+  app.get('/deaths', (req, res) => {
+    axios
+      .get('https://breakingbadapi.com/api/deaths')
+      .then((response) => {
+        console.log(response);
+        res.render('deaths', { deaths: response.data });
+      })
+      .catch((err) => console.log(err));
+  });
+};
--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// static css
+app.use(express.static('public'));
+
+// parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set handlebars
+const exphbs = require('express-handlebars');
+app.set('view engine', 'handlebars');
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main'
+  })
+);
+
+// Require routes
+require('./routes/html')(app);
+
 app.listen(PORT, (res) => {
   console.log(`App listening on http://localhost:${PORT}`);
 });
